d83b944ca100e786afcb905df53df6a8792c3242
DangerousCoder06 <kumar.vaibhav6122005@gmail.com>
2025-09-02 02:01:19 +0530

    optimize background for large screen

--- src/app/sponsors/page.js
+++ src/app/sponsors/page.js
@@ -3,14 +3,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="main w-screen h-screen">
-        <Image src="./sponsors-bg.svg" fill className="object-cover z-0 relative" alt="background" />
-        <div className="bg-black z-10 absolute inset-0 w-screen h-screen opacity-30"></div>
+    <div className="main relative w-screen h-screen">
+        <Image
+        src="/sponsors-bg.svg"
+        alt="Cropped Image"
+        fill
+        className="object-cover"
+      />
+        <div className="bg-black z-10 absolute inset-0 w-screen h-screen opacity-10"></div>
         <nav className="relative z-20">
 
         </nav>
         <div className="flex justify-center items-center h-screen">
-            <section className="relative z-20 w-[380px] h-[480px] bg-white/8 rounded-2xl backdrop-blur-md shadow-lg">
+            <section className="relative z-20 w-[300px] h-[400px] bg-white/8 rounded-2xl backdrop-blur-md shadow-lg">
 
             </section>
 
