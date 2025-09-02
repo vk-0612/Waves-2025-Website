05fe68699286f721f84c796ccd412ebdd0cc65f9
DangerousCoder06 <kumar.vaibhav6122005@gmail.com>
2025-09-02 16:10:43 +0530

    prevent overflow

--- src/app/sponsors/page.js
+++ src/app/sponsors/page.js
@@ -3,17 +3,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="main relative w-screen h-screen">
+    <div className="main relative w-screen h-screen overflow-hidden">
         <Image
         src="/sponsors-bg.svg"
-        alt="Cropped Image"
+        alt="Background"
         fill
         className="object-cover"
       />
-        <div className="bg-black z-10 absolute inset-0 w-screen h-screen opacity-10"></div>
-        <nav className="relative z-20">
-
-        </nav>
+        <div className="bg-black z-10 absolute inset-0 w-screen h-screen opacity-25"></div>
         <div className="flex justify-center items-center h-screen">
             <section className="relative z-20 w-[300px] h-[400px] bg-white/8 rounded-2xl backdrop-blur-md shadow-lg">
 
